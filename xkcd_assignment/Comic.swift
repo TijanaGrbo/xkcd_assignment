1337d66c81238a4b349d592766d9117f053fdeb8
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-01 20:18:18 +0100

    Add Comic model

---
+++ xkcd_assignment/Comic.swift
@@ -0,0 +1,34 @@
+//
+//  Comic.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 01/03/2023.
+//
+
+import Foundation
+
+struct Comic: Codable, Identifiable, Equatable {
+
+    var id: Int {
+        return num
+    }
+    
+    let num: Int
+    let transcript: String
+    let title: String
+    let img: String
+    
+    var imgURL: URL? {
+        return URL(string: img)
+    }
+}
+
+// full model parameters. Not needed for now.
+//let alt: String
+//let day: String
+//let month: String
+//let year: String
+//let link: String
+//let news: String
+//let safeTitle: String
+//let transcript: String
