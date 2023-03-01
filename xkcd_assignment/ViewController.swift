73a5acedf7ff7414d53efd302a7a8604261495e9
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-01 13:21:08 +0100

    Initial Commit

---
+++ xkcd_assignment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 01/03/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
