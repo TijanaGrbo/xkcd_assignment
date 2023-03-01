68f1a6acb2739a4fe93cc232e68d18e7d1f6b1fc
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-01 20:24:08 +0100

    Add MainCoordinator

---
+++ xkcd_assignment/MainCoordinator.swift
@@ -0,0 +1,19 @@
+//
+//  MainCoordinator.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 01/03/2023.
+//
+
+import UIKit
+
+class MainCoordinator {
+    var navController: UINavigationController
+    
+    init(navController: UINavigationController) {
+        self.navController = navController
+    }
+    
+    func start() {
+    }
+}
