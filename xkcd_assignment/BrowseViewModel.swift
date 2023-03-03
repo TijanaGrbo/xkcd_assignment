6e0da28aff390054744e5fecf0bb0023032ca4ec
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-03 20:39:59 +0100

    Add Core Data + methods that connect data and VC

--- xkcd_assignment/BrowseViewModel.swift
+++ xkcd_assignment/BrowseViewModel.swift
@@ -7,10 +7,14 @@
 
 import Foundation
 import Kingfisher
+import UIKit
+import CoreData
 
 class BrowseViewModel {
     @Published var comic: Comic?
+    
     var latestComicNum: Int?
+    var storageProvider: StorageProvider
     
     func getPreviousComic() async {
         guard var currentComicNum = comic?.num, let highestComicNum = latestComicNum, currentComicNum <= highestComicNum else { return }
@@ -27,5 +31,40 @@ class BrowseViewModel {
         guard var currentComicNum = comic?.num, let highestComicNum = latestComicNum, currentComicNum < highestComicNum else { return }
         currentComicNum += 1
         comic = try? await ComicAPI.shared.getComic(withNum: currentComicNum)
+    }
+    
+    init(storageProvider: StorageProvider) {
+        self.storageProvider = storageProvider
+    }
+}
+
+extension BrowseViewModel {
+    func saveToFavourites(withImage image: UIImage) {
+        guard let comic = comic else { return }
+        storageProvider.saveComicToFavourites(comic, withImage: image)
+        loadFromFavourites()
+    }
+    
+    func loadFromFavourites() {
+        _ = storageProvider.getFavouriteComics()
+    }
+    
+    func favouriteButtonTapped(comicImage: UIImage) {
+        guard let comicNum = comic?.num else { return }
+        if storageProvider.isFavourite(comicNum: comicNum) {
+            storageProvider.removeComicFromFavourites(comicNum)
+        } else {
+            guard let comic = comic else { return }
+            storageProvider.saveComicToFavourites(comic, withImage: comicImage)
+        }
+    }
+    
+    func checkIfLiked() -> Bool {
+        guard let comicNum = comic?.num else { return false }
+        if storageProvider.isFavourite(comicNum: comicNum) {
+            return true
+        } else {
+            return false
+        }
     }
 }
