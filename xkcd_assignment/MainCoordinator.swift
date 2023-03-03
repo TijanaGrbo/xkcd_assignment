166b84cbe64306f3bc0d85ec046d3c7b4537c0fa
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-03 20:47:23 +0100

    Initialize newly created VCs in MainCoordinator

--- xkcd_assignment/MainCoordinator.swift
+++ xkcd_assignment/MainCoordinator.swift
@@ -17,8 +17,21 @@ class MainCoordinator {
     }
     
     func start() {
-        let vm = BrowseViewModel()
-        let vc = BrowseVC(coordinator: self, viewModel: vm)
-        navController.pushViewController(vc, animated: true)
+        let browseVM = BrowseViewModel(storageProvider: storageProvider)
+        let browseVC = BrowseVC(coordinator: self, viewModel: browseVM)
+        
+        let favouritesVM = BrowseViewModel(storageProvider: storageProvider)
+        let fabouritesVC = BrowseVC(coordinator: self, viewModel: favouritesVM)
+        
+        let searchVM = SearchViewModel()
+        let searchVC = SearchVC(searchViewModel: searchVM)
+        
+        let tabBar = TabBarVC()
+        tabBar.viewControllers = [browseVC, fabouritesVC, searchVC]
+        tabBar.tabBar.items?[0].title = "Browse"
+        tabBar.tabBar.items?[1].title = "Favourites"
+        tabBar.tabBar.items?[2].title = "Search"
+        
+        navController.pushViewController(tabBar, animated: true)
     }
 }
