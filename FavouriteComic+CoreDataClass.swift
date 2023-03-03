ca8cc5d91f7711b08793d287cca4046586546064
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-03 20:49:11 +0100

    Generate CD class and properties files

---
+++ FavouriteComic+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  FavouriteComic+CoreDataClass.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 02/03/2023.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(FavouriteComic)
+public class FavouriteComic: NSManagedObject {
+
+}
---
+++ FavouriteComic+CoreDataProperties.swift
@@ -0,0 +1,28 @@
+//
+//  FavouriteComic+CoreDataProperties.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 02/03/2023.
+//
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+extension FavouriteComic {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<FavouriteComic> {
+        return NSFetchRequest<FavouriteComic>(entityName: "FavouriteComic")
+    }
+
+    @NSManaged public var image: UIImage?
+    @NSManaged public var num: Int16
+    @NSManaged public var title: String?
+    @NSManaged public var alt: String?
+
+}
+
+extension FavouriteComic : Identifiable {
+
+}
