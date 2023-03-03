4df21b7da33900110fe17b166280b847485e1687
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-03 20:46:39 +0100

    Add TabBarVC

---
+++ xkcd_assignment/TabBarVC.swift
@@ -0,0 +1,15 @@
+//
+//  TabBarVC.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 02/03/2023.
+//
+
+import UIKit
+
+class TabBarVC: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
