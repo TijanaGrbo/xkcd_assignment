712c3bcc3f36f9fbc212b569fb08b9249c0a1347
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-04 11:39:32 +0100

    Add ComicViewModelProtocol

---
+++ xkcd_assignment/ComicViewModelProtocol.swift
@@ -0,0 +1,44 @@
+//
+//  ComicViewModelProtocol.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 03/03/2023.
+//
+
+import Foundation
+import UIKit
+
+protocol ComicViewModel {
+    func getPreviousComic() async
+    func getLatestComic() async
+    func getNextComic() async
+    func comicImageURL() -> URL?
+    func comicImage() -> UIImage?
+    func saveToFavourites(withImage image: UIImage)
+    func loadFromFavourites()
+    func favouriteButtonTapped(comicImage: UIImage)
+    func checkIfLiked() -> Bool
+    func setComicTitle() -> String
+    func setComicNum() -> String
+    func getPreviousButtonState() -> Bool
+    func getLatestButtonState() -> Bool
+    func getNextButtonState() -> Bool
+}
+
+extension ComicViewModel {
+    func comicImageURL() -> URL? {
+        return nil
+    }
+    
+    func comicImage() -> UIImage? {
+        return UIImage()
+    }
+    
+    func saveToFavourites(withImage image: UIImage) {}
+    
+    func favouriteButtonTapped(comicImage: UIImage) {}
+    
+    func checkIfLiked() -> Bool {
+        return false
+    }
+}
