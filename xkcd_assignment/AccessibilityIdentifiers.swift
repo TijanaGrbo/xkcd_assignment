5bc3d39231627cf0a74f1f2f3cec41bbaacb8364
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-04 18:37:51 +0100

    Create AccessibilityIdentifiers enum

---
+++ xkcd_assignment/AccessibilityIdentifiers.swift
@@ -0,0 +1,21 @@
+//
+//  AccessibilityIdentifiers.swift
+//  xkcd_assignment
+//
+//  Created by Tijana on 04/03/2023.
+//
+
+import Foundation
+
+enum AccessibilityIdentifier: String {
+    case title
+    case comicNum
+    case comicImage
+    case prevButton
+    case latestButton
+    case nextButton
+    case like
+    case tabBarBrowse
+    case tabBarFavorites
+    case tabBarSearch
+}
