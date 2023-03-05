21b7285b1c6ee79069cdf61d4e0a8e01d2f91501
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-05 15:05:26 +0100

    Add custom property to UIColor extension

--- xkcd_assignment/Extensions/UIColor+ex.swift
+++ xkcd_assignment/Extensions/UIColor+ex.swift
@@ -12,4 +12,5 @@ extension UIColor {
     static let middleGradientColor = UIColor(displayP3Red: 217/255, green: 163/255, blue: 203/255, alpha: 1).cgColor
     static let topGradientColor = UIColor(displayP3Red: 227/255, green: 223/255, blue: 233/255, alpha: 1).cgColor
     static let favouriteButtonColor = UIColor(red: 199/255.0, green: 30/255.0, blue: 30/255.0, alpha: 1.0)
+    static let noContentTextColor = UIColor(red: 47/255.0, green: 47/255.0, blue: 47/255.0, alpha: 1.0)
 }
