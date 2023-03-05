76ca4ff367f4d32adce2feaf1dfcba419f8f9394
Tijana Grbo <39621438+TijanaGrbo@users.noreply.github.com>
2023-03-05 11:18:25 +0100

    Make coordinator and storageProvider private

--- xkcd_assignment/Base/SceneDelegate.swift
+++ xkcd_assignment/Base/SceneDelegate.swift
@@ -10,15 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var coordinator: MainCoordinator!
-    let storageProvider = StorageProvider()
+    private(set) var coordinator: MainCoordinator?
+    private let storageProvider = StorageProvider()
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         self.window = UIWindow(windowScene: windowScene)
         let navController = UINavigationController()
         self.coordinator = MainCoordinator(navController: navController, storageProvider: storageProvider)
-        self.coordinator.start()
+        self.coordinator?.start()
         self.window?.rootViewController = navController
         self.window?.makeKeyAndVisible()
     }
